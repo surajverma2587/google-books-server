fe9848d57e50c797c7f746362c7930785f5580e9
Suraj Verma <surajverma@rentalcars.com>
2020-09-06 15:34:39 +0100

    wip

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,43 @@
 const express = require("express");
+const axios = require("axios");
 
 const db = require("../models");
 
+const GOOGLE_URL = "https://www.googleapis.com/books/v1/volumes";
+
 const apiRouter = express.Router();
 
-const getAllBooks = async (req, res) => {
-  const books = await db.Book.find({});
-  res.json({
-    results: books,
+const transformGoogleBooks = (googleBooks = []) => {
+  return googleBooks.map((googleBook) => {
+    return {
+      title: googleBook.volumeInfo.title,
+      subTitle: googleBook.volumeInfo.subTitle,
+      description: googleBook.volumeInfo.description,
+      authors: googleBook.volumeInfo.authors,
+      image: googleBook.volumeInfo.imageLinks.thumbnail,
+      link: googleBook.volumeInfo.previewLink,
+    };
   });
 };
 
-apiRouter.get("/books", getAllBooks);
+const getBooksFromGoogle = async (req, res) => {
+  try {
+    const { searchTerm } = req.body;
+
+    const { data } = await axios.get(GOOGLE_URL, { params: { q: searchTerm } });
+
+    const books = transformGoogleBooks(data.items);
+
+    res.json({
+      results: books,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+apiRouter.post("/books", getBooksFromGoogle);
 
 module.exports = apiRouter;
