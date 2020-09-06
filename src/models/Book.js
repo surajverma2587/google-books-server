2b630f4eed72ee46d65d42dc94eb153cce3be0c2
Suraj Verma <surajverma@rentalcars.com>
2020-09-06 14:17:50 +0100

    update readme

--- src/models/Book.js
+++ src/models/Book.js
@@ -2,7 +2,32 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const schema = new Schema({});
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subTitle: {
+    type: String,
+    required: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: [String],
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
 
 const Book = mongoose.model("Book", schema);
 
--- src/seed.js
+++ src/seed.js
@@ -1,11 +1,44 @@
 const mongoose = require("mongoose");
 
-const db = require("../models");
+const db = require("./models");
 const { DB_URI, MONGOOSE_OPTIONS } = require("./config");
 
 mongoose.connect(DB_URI, MONGOOSE_OPTIONS);
 
-const books = [];
+const books = [
+  {
+    authors: ["Suzanne Collins"],
+    description:
+      "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+    image:
+      "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    link:
+      "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+    title: "The Hunger Games",
+    subTitle: "War between the districts",
+  },
+  {
+    authors: ["Suzanne Collins"],
+    description:
+      "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+    image:
+      "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    link:
+      "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+    title: "The Hunger Games",
+  },
+  {
+    authors: ["Suzanne Collins"],
+    description:
+      "Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+    image:
+      "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    link:
+      "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+    title: "The Hunger Games",
+    subTitle: "War between the districts",
+  },
+];
 
 db.Book.deleteMany({})
   .then(() => db.Book.collection.insertMany(books))
